8feb8e985aff3fc7892b3e3cb873fbe710bb80b4
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-08-08 21:42:41 -0500

    Grid centrada y footer

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import datos from './data/harrypotter/harrypotterdata.js';
 //import data from './data/harrypotter/harrypotterdata.js'; //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+const allCharacters= datos.characters;
+const generadorHTML=(character)=>{
+  return `<div class='charactersStyle'>
+            <p>${character.name}</p>
+          </div>`
+}
 
+let todoelHTML =''
 
 //document.getElementById("character").innerHTML=books
+allCharacters.forEach(oneCharacter =>todoelHTML+=generadorHTML(oneCharacter));
 
 //console.log(datos)
 //console.log(datos.characters[0].name)
@@ -16,4 +24,5 @@ import datos from './data/harrypotter/harrypotterdata.js';
 
 datos.characters.forEach(name => document.getElementById("showCharacter").innerHTML= name["name"]);
 
-//datos.characters.forEach(name => document.getElementById("showCharacter").innerHTML= {divCreator(name["name"])});
+//datos.characters.forEach(name => document.getElementById("showCharacter").innerHTML= {divCreator(name["name"])}); 
+document.getElementById("showCharacter").innerHTML= todoelHTML;
