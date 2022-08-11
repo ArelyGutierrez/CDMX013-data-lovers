e021787e3aa116b82adb98ac89041d7ca43d2a63
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-08-11 15:32:33 -0500

    modal en todos pero con algunas repeticiones en la info

--- src/main.js
+++ src/main.js
@@ -1,52 +1,56 @@
 //import { userStory1 } from './data.js'; //import { example } from './data.js';
-import datos from './data/harrypotter/harrypotterdata.js';
-
+import datos from "./data/harrypotter/harrypotterdata.js";
 const allCharacters = datos.characters;
 const generadorHTML = (character) => {
- //*let newDiv = document.createElement("div")*/
-  let characters =
-    `<button id="charactersStyle" class="charactersStyle">
+  //*let newDiv = document.createElement("div")*/
+  let characters = `<button id="${character.id}" class="charactersStyle">
             <p>${character.name}</p>
           </button>
-
-          <div id="myModal" class="modal"> 
+          <div id="myModal" class="modal">
               <div class="modal-content">
               <span class="close">&times;</span>
-              <p>Name: ${character.name}</p>
-              <p>Birth: ${character.birth}</p>
-              <p>Species: ${character.species}</p>
-              <p>House: ${character.house}</p>
+              <div id="characterData">
+              </div>
               </div>
           </div>`;
-  
   return characters;
-}
-
-let todoelHTML = ''
-allCharacters.forEach(oneCharacter => todoelHTML += generadorHTML(oneCharacter));
+};
+let todoelHTML = "";
+allCharacters.forEach(
+  (oneCharacter) => (todoelHTML += generadorHTML(oneCharacter))
+);
 document.getElementById("showCharacter").innerHTML = todoelHTML;
-
-
-
-var modal = document.getElementById("myModal"); /* Get the modal*/ 
-var btn = document.getElementById("charactersStyle");/*Get the div character that opens the modal*/ 
-var span = document.getElementsByClassName("close")[0]; /*Get the div character that opens the modal*/
-
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the div character that opens the modal
+var btn = document.getElementsByClassName("charactersStyle");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+const insertDataToModal = (id) => {
+  const character = allCharacters.find(
+    (character) => character.id === Number(id)
+  );
+  const characterData = document.getElementById("characterData");
+  characterData.innerHTML = `<p>Name: ${character.name}</p>
+   <p>Birth: ${character.birth}</p>
+   <p>Species: ${character.species}</p>
+   <p>House: ${character.house}</p>`;
+};
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
+for (let i = 0; i < btn.length; i++) {
+  btn[i].onclick = function (e) {
+    const id = e.srcElement.id;
+    modal.style.display = "block";
+    insertDataToModal(id);
+  };
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
-}
-
+};
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}  
-
-
+};
